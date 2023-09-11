76d8945f5748740806abebd8f4978ce6aa20055b
Nikunj Buddhbhatti <nikunjb11@gmail.com>
2023-09-11 12:58:32 +0530

    Dev counter added

--- lib/main.dart
+++ lib/main.dart
@@ -99,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              'Dev Counter : $_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
